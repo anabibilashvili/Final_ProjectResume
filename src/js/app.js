b932e7f52e94395131553954b062318908de1129
Ana Bibilashvili <94191785+anabibilashvili@users.noreply.github.com>
2023-08-18 15:26:48 +0400

    add service section part

--- src/js/app.js
+++ src/js/app.js
@@ -18,3 +18,32 @@ function showSlides() {
 }
 
 showSlides();
+
+document.addEventListener("scroll", updateProgress);
+
+function updateProgress() {
+  const htmlProgress = document.getElementById("html-progress");
+  const cssProgress = document.getElementById("css-progress");
+  const jsProgress = document.getElementById("js-progress");
+
+  const htmlPercent = 50;
+  const cssPercent = 70;
+  const jsPercent = 90;
+
+  const scrollY = window.scrollY;
+  const windowHeight = window.innerHeight;
+  const pageHeight = document.documentElement.scrollHeight;
+
+  const htmlWidth = (scrollY / (pageHeight - windowHeight)) * htmlPercent;
+  const cssWidth = (scrollY / (pageHeight - windowHeight)) * cssPercent;
+  const jsWidth = (scrollY / (pageHeight - windowHeight)) * jsPercent;
+
+  htmlProgress.style.width = `${htmlWidth}%`;
+  htmlProgress.textContent = `HTML (${Math.round(htmlWidth)}) %`;
+
+  cssProgress.style.width = `${cssWidth}%`;
+  cssProgress.textContent = `CSS ( ${Math.round(cssWidth)}) %`;
+
+  jsProgress.style.width = `${jsWidth}%`;
+  jsProgress.textContent = `JS (${Math.round(jsWidth)}) %`;
+}
