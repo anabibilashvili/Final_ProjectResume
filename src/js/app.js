0fec4ac3c88edbdd4ed74f4eee82f882221a8f6f
Ana Bibilashvili <94191785+anabibilashvili@users.noreply.github.com>
2023-09-01 22:39:04 +0400

    change format with code prittier

--- src/js/app.js
+++ src/js/app.js
@@ -20,15 +20,18 @@ function showSlides() {
 showSlides();
 
 const skills = [
-  { name: 'html', percent: 80 },
-  { name: 'css', percent: 70 },
-  { name: 'js', percent: 50 },
+  { name: "html", percent: 80 },
+  { name: "css", percent: 70 },
+  { name: "js", percent: 50 },
 ];
 
 function animateSkillBars() {
-  skills.forEach(skill => {
-    const skillProgress = document.querySelector(`.skill-progress.${skill.name}`);
-    const targetWidth = (skill.percent / 100) * skillProgress.parentElement.offsetWidth;
+  skills.forEach((skill) => {
+    const skillProgress = document.querySelector(
+      `.skill-progress.${skill.name}`
+    );
+    const targetWidth =
+      (skill.percent / 100) * skillProgress.parentElement.offsetWidth;
 
     let currentWidth = 0;
     const animationInterval = setInterval(() => {
@@ -60,71 +63,63 @@ function showSlide(index) {
   slideButtons[currentSlideIndex].classList.add("active");
 }
 
-
 for (let i = 0; i < slideButtons.length; i++) {
   slideButtons[i].addEventListener("click", () => showSlide(i));
 }
 showSlide(currentSlideIndex);
 
+document.addEventListener("DOMContentLoaded", function () {
+  const sendButton = document.getElementById("submit");
+  const modal = document.getElementById("modal");
 
-document.addEventListener('DOMContentLoaded', function() {
-  const sendButton = document.getElementById('submit');
-  const modal = document.getElementById('modal');
-  
-
-
-
-  sendButton.addEventListener('click', function() {
-    const name = document.getElementById('name').value;
-    const email = document.getElementById('email').value;
-    const website = document.getElementById('website').value;
-    const message = document.getElementById('messageId').value;
+  sendButton.addEventListener("click", function () {
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const website = document.getElementById("website").value;
+    const message = document.getElementById("messageId").value;
 
     const formData = {
       name: name,
       email: email,
       website: website,
-      message: message
+      message: message,
     };
 
-    fetch('https://borjomi.loremipsum.ge/api/send-message', {
-      method: 'POST',
+    fetch("https://borjomi.loremipsum.ge/api/send-message", {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json'
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify(formData)
+      body: JSON.stringify(formData),
     })
-    .then(response => response.json())
-    .then(data => {
-      if (data.status === 1) {
-        modal.style.display = 'block'; // Show success modal
-      }
-    })
-    .catch(error => {
-      console.error('Error sending message:', error);
-    });
-  });
-});
-
-document.addEventListener('DOMContentLoaded', function () {
-  const filterCategory = document.querySelectorAll('.chooseCategory');
-  const items = document.querySelectorAll('.item');
-
-  filterCategory.forEach(button => {
-      button.addEventListener('click', function () {
-          const category = this.getAttribute('data-category');
-
-          items.forEach(item => {
-              const itemCategory = item.getAttribute('data-category');
-              if (category === 'all' || category === itemCategory) {
-                  item.style.display = 'block';
-              } else {
-                  item.style.display = 'none';
-              }
-          });
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.status === 1) {
+          modal.style.display = "block"; // Show success modal
+        }
+      })
+      .catch((error) => {
+        console.error("Error sending message:", error);
       });
   });
 });
 
+document.addEventListener("DOMContentLoaded", function () {
+  const filterCategory = document.querySelectorAll(".chooseCategory");
+  const items = document.querySelectorAll(".item");
 
+  filterCategory.forEach((button) => {
+    button.addEventListener("click", function () {
+      const category = this.getAttribute("data-category");
 
+      items.forEach((item) => {
+        const itemCategory = item.getAttribute("data-category");
+        if (category === "all" || category === itemCategory) {
+          item.style.display = "block";
+        } else {
+          item.style.display = "none";
+        }
+      });
+    });
+  });
+});
